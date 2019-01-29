bac71304304fd1b9c0861042c2b0ce0b1a7d5b35
steven <steven@pin2wall.com>
2019-01-29 18:14:38 +0800

    create project

---
+++ server.js
@@ -0,0 +1,19 @@
+var dotenv = require('dotenv').config();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+// app.get('/', function(req, res){
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', function(socket){
+    socket.on('chat_message', function(msg){
+        io.emit('chat_message', msg);
+    });
+});
+
+http.listen(port, function(){
+    console.log('listening on *:' + port);
+});
