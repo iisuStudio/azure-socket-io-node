f345d577661f467571256317ceccde7d2378f89d
steven <steven@pin2wall.com>
2019-01-30 11:19:01 +0800

    use dotenv-extended package replace dotenv package

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ var methodOverride = require('method-override');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
-var dotenv = require('dotenv');
+var dotenv = require('dotenv-extended');
 
 var app = express();
 var server = http.Server(app);
 var io = socket(server);
 
-dotenv.config();
+dotenv.load();
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
