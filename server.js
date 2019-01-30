e60a2f094d985c06f2fa8078dcd9f1b9ca4a4333
steven <steven@pin2wall.com>
2019-01-30 13:03:40 +0800

    testing on azure site

--- server.js
+++ server.js
@@ -21,40 +21,40 @@ var io = socket(server);
 
 // dotenv.load();
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(methodOverride());
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: 'uwotm8'
-}));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(logger('dev'));
+// app.use(methodOverride());
+// app.use(session({
+//     resave: true,
+//     saveUninitialized: true,
+//     secret: 'uwotm8'
+// }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-if(process.env.NODE_ENV === 'production') {
-    app.set('port', 80);
-    // additional prod environment configuration
-}
-
-if(process.env.NODE_ENV === 'development') {
-    app.use(errorHandler());
-    // additional develop environment configuration
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.set('port', 80);
+//     // additional prod environment configuration
+// }
+//
+// if(process.env.NODE_ENV === 'development') {
+//     app.use(errorHandler());
+//     // additional develop environment configuration
+// }
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-io.on('connection', function(socket){
-    socket.on('chat_message', function(msg){
-        io.emit('chat_message', msg);
-    });
-});
+// io.on('connection', function(socket){
+//     socket.on('chat_message', function(msg){
+//         io.emit('chat_message', msg);
+//     });
+// });
 
 server.listen(app.get('port'), function(){
     console.log('listening on *:' + app.get('port'));
